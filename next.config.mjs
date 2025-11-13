f55d108a3c28d25aa6b152ef652d7fbf476b7805
yassinali <mozsoftware@gmail.com>
2025-11-13 07:56:05 +0200

    updated

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), '@prisma/client'];
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
