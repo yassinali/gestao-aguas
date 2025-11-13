c43b1c755d9802b246240cbfe57397cfa6b07fa3
yassinali <mozsoftware@gmail.com>
2025-11-13 08:38:23 +0200

    updated

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,9 @@
-import { PrismaClient } from "@prisma/client"
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query"] : [],
-  })
+  globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
